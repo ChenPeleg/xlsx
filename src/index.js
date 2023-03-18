9db431513f876eedb5b09639b19ad87315561f39
chenp <chenpeleg@gmail.com>
2023-03-18 18:29:31 +0200

    added async behavior

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { runZipper } from './archive/zipper.js';
 import { copyFiles } from './cmd/copyFiles.js';
 
-const runApp = () => {
-  copyFiles('xlsx/base', 'temp');
+const runApp = async () => {
+  await copyFiles('xlsx/base', 'temp');
   runZipper();
 };
 runApp();
