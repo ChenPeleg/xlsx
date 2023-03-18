c75cc530d888d321d9734c5e01f96d83ec46313b
chenp <chenpeleg@gmail.com>
2023-03-18 18:25:52 +0200

    added copy files

--- src/archive/zipper.js
+++ src/archive/zipper.js
@@ -1,8 +1,10 @@
-import { execPromise } from './execPromise.js';
+import { execPromise } from '../utils/execPromise.js';
 
 export const runZipper = async () => {
   const command1 = `tar -rf 'myfiles.tar' temp`;
   const command = `tar -cf archive.zip *`;
-  const res = await execPromise(command);
+  const res = await execPromise(command, {
+    cwd: 'temp',
+  });
   console.log(res);
 };
