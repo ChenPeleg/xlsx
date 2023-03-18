7bf6be88ad96f7c754ee3a057cdc8cf2adc39212
chenp <chenpeleg@gmail.com>
2023-03-18 18:27:28 +0200

    changed dir name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { runZipper } from './archive/zipper.js';
+import { copyFiles } from './cmd/copyFiles.js';
 
 const runApp = () => {
+  copyFiles('xlsx/base', 'temp');
   runZipper();
 };
 runApp();
