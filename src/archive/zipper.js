6687346285f7695815eb19a8431cb8c04f72cb1d
chenp <chenpeleg@gmail.com>
2023-03-18 20:20:21 +0200

    able to zip file

--- src/archive/zipper.js
+++ src/archive/zipper.js
@@ -1,8 +1,8 @@
 import { execPromise } from '../utils/execPromise.js';
 
 /** @param {string} fileName */
-export const runZipper = async (fileName = 'workbook.xlsx') => {
-  const command = `tar -cf  ${fileName} *`;
+export const runZipper = async (fileName = '../out/workbook.zip') => {
+  const command = `tar -a -c -f  ${fileName} *`;
   const res = await execPromise(command, {
     cwd: 'temp',
   });
