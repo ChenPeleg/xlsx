9a71ad003c04d6352f9aa6891c694476affcad9c
chenp <chenpeleg@gmail.com>
2023-03-18 20:39:22 +0200

    can create excel file in windows

--- src/archive/zipper.js
+++ src/archive/zipper.js
@@ -2,11 +2,11 @@ import { renameFile } from '../cmd/renameFile.js';
 import { execPromise } from '../utils/execPromise.js';
 
 /** @param {string} fileName */
-export const runZipper = async (fileName = '../out/workbook.zip') => {
-  const psCommand = `Compress-Archive -Path * -DestinationPath ../out/archive.zip`;
+export const runZipper = async (fileName = 'workbook') => {
+  const psCommand = `Compress-Archive -Path * -DestinationPath ../out/${fileName}.zip`;
   const res = await execPromise(psCommand, {
     cwd: 'temp',
     shell: 'powershell.exe',
   });
-  // await renameFile('out/workbook.zip', 'out/workbook.xlsx');
+  await renameFile(`out/${fileName}.zip`, `out/${fileName}.xlsx`);
 };
