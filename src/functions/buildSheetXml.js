6a8c242c812d0d81ebc7b525f75353e58efdb87a
chenp <chenpeleg@gmail.com>
2023-03-30 17:01:30 +0300

    added build function for the sheet

---
+++ src/functions/buildSheetXml.js
@@ -0,0 +1,13 @@
+import { Worksheet } from '../class/worksheet.js';
+
+/**
+ * @param {Worksheet} worksheet
+ * @returns
+ */
+export const buildSheetXml = (worksheet) => {
+  const rows = worksheet.rows;
+  const rowsLength = rows.length || 3;
+  const maxColumns = Math.max(...rows.map((r) => r.cells.length)) || 3;
+
+  return '';
+};
---
+++ src/functions/buildWorkbook.js
@@ -0,0 +1,1 @@
+export const buildWorksheet = (workbook) => {};
