5b974cf9ed095f0633e58a6a0aefea1c5ad6af35
chenp <chenpeleg@gmail.com>
2023-03-30 17:42:20 +0300

    added excel data rendering

--- src/data/addData.js
+++ src/data/addData.js
@@ -1,1 +1,13 @@
-export const addData = async (data, config) => {};
+import { Cell } from '../class/cell.js';
+import { Row } from '../class/row.js';
+import { Workbook } from '../class/workbook.js';
+import { Worksheet } from '../class/worksheet.js';
+import { buildSheetXml } from '../functions/buildSheetXml.js';
+
+export const addData = async (data, config) => {
+  const cells = [new Cell(4234), new Cell(772), new Cell(12)];
+  const rows = [new Row(cells), new Row(cells), new Row(cells)];
+  const worksheet = new Worksheet(rows);
+  const sheet1 = buildSheetXml(worksheet);
+  console.log(sheet1);
+};
