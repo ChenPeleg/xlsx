241fded4d599b5039837e129628c2e920f5110ca
chenp <chenpeleg@gmail.com>
2023-03-30 13:40:14 +0300

    added the file add data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { runZipper } from './archive/zipper.js';
 import { copyFiles } from './cmd/copyFiles.js';
 import { deleteFiles } from './cmd/deleteFiles.js';
+import { addData } from './data/addData.js';
 
 const runApp = async () => {
   await copyFiles('xlsx/base', 'temp');
+  await addData();
   await deleteFiles('out');
   await runZipper();
 };
