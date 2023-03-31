1905b152cf117b6c7e456a88ebb9b59be06583e5
chenp <chenpeleg@gmail.com>
2023-03-31 17:14:49 +0300

    entering text works

--- src/data/editData.js
+++ src/data/editData.js
@@ -10,7 +10,7 @@ import { replaceInFile } from '../utils/replaceInFile.js';
  * @param {import('../types/config.type.js').XlsConfig} config
  */
 export const editData = async (data, config) => {
-  const cells = [new Cell(4234), new Cell(772), new Cell(12)];
+  const cells = [new Cell(4234), new Cell('I am a text'), new Cell(12)];
   const rows = [new Row(cells), new Row(cells), new Row(cells)];
   const worksheet = new Worksheet(rows);
   const sheet1 = buildSheetXml(worksheet);
