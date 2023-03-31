1b532a3d423521fa2e343659df01af586786f563
chenp <chenpeleg@gmail.com>
2023-03-31 18:24:00 +0300

    test problem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import { resolve } from 'path';
-import { runZipper } from './archive/zipper.js';
-import { copyFiles } from './cmd/copyFiles.js';
-import { deleteFiles } from './cmd/deleteFiles.js';
-import { editData } from './data/editData.js';
+import { resolve } from "path";
+import { runZipper } from "./archive/zipper.js";
+import { copyFiles } from "./cmd/copyFiles.js";
+import { deleteFiles } from "./cmd/deleteFiles.js";
+import { editData } from "./data/editData.js";
 
 /** @param {{ tempDir: string }} config */
 
 export const runApp = async (data, config) => {
-  // await copyFiles(resolve('xlsx', 'base'), config.tempDir);
-  // await editData(data, config);
-  // await deleteFiles('out');
-  // await runZipper();
+  const tempDir = config?.tempDir || "temp";
+
+  await deleteFiles(resolve(tempDir));
+  await copyFiles(resolve("xlsx", "base"), tempDir);
+  await editData(data, config);
+  await deleteFiles("out");
+  await runZipper();
 };
-runApp(null, { tempDir: 'temp' });
-console.log('success!');
+runApp(null, { tempDir: "temp" });
+console.log("success!");
