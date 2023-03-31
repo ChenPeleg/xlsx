6434ab8daf3afcc3a1da8b7204c55f1f1a8d0395
chenp <chenpeleg@gmail.com>
2023-03-31 17:43:13 +0300

    one test passes

---
+++ test/integration/test1.test.js
@@ -0,0 +1,8 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+
+describe('first mock test', () => {
+  it('simple test', () => {
+    assert.equal(6, 6);
+  });
+});
