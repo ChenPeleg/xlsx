076c0d6875b4995fa7caae4def70103096dc030d
chenp <chenpeleg@gmail.com>
2023-04-02 18:38:22 +0300

    changed file system paths

--- src/archive/zipper.js
+++ src/archive/zipper.js
@@ -1,12 +1,16 @@
-import { renameFile } from '../cmd/renameFile.js';
-import { execPromise } from '../utils/execPromise.js';
+import { resolve } from "node:path";
+import { renameFile } from "../cmd/renameFile.js";
+import { execPromise } from "../utils/execPromise.js";
 
 /** @param {string} fileName */
-export const runZipper = async (fileName = 'workbook') => {
-  const psCommand = `Compress-Archive -Path * -DestinationPath ../out/${fileName}.zip`;
+export const runZipper = async (fileName = "workbook") => {
+  const psCommand = `Compress-Archive -Path * -DestinationPath ${resolve(
+    "../",
+    "out"
+  )}${fileName}.zip`;
   const res = await execPromise(psCommand, {
-    cwd: 'temp',
-    shell: 'powershell.exe',
+    cwd: "temp",
+    shell: "powershell.exe",
   });
   await renameFile(`out/${fileName}.zip`, `out/${fileName}.xlsx`);
 };
