2f4ffc6f7c579624775b2b360973d352676f665a
chenp <chenpeleg@gmail.com>
2023-04-02 18:26:54 +0300

    separated app from runner

---
+++ src/core/main.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { runZipper } from "../archive/zipper.js";
+import { copyFiles } from "../cmd/copyFiles.js";
+import { deleteFiles } from "../cmd/deleteFiles.js";
+import { editData } from "../data/editData.js";
+
+/** @param {{ tempDir: string }} config */
+
+export const runApp = async (data, config) => {
+  const tempDir = config?.tempDir || "temp";
+
+  await deleteFiles(resolve(tempDir));
+  await copyFiles(resolve("xlsx", "base"), tempDir);
+  await editData(data, config);
+  await deleteFiles("out");
+  await runZipper();
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-import { resolve } from "path";
-import { runZipper } from "./archive/zipper.js";
-import { copyFiles } from "./cmd/copyFiles.js";
-import { deleteFiles } from "./cmd/deleteFiles.js";
-import { editData } from "./data/editData.js";
+import { runApp } from "./core/main.js";
 
-/** @param {{ tempDir: string }} config */
-
-export const runApp = async (data, config) => {
-  const tempDir = config?.tempDir || "temp";
-
-  await deleteFiles(resolve(tempDir));
-  await copyFiles(resolve("xlsx", "base"), tempDir);
-  await editData(data, config);
-  await deleteFiles("out");
-  await runZipper();
-};
 runApp(null, { tempDir: "temp" });
 console.log("success!");
