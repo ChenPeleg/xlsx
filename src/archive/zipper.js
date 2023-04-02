e300947504b75a52e8a40ff4759ac9dc24110dfd
chenp <chenpeleg@gmail.com>
2023-04-02 19:43:55 +0300

    added linux support

--- src/archive/zipper.js
+++ src/archive/zipper.js
@@ -1,15 +1,25 @@
 import { resolve } from "node:path";
 import { renameFile } from "../cmd/renameFile.js";
 import { execPromise } from "../utils/execPromise.js";
+import { platform } from "node:os";
 
 /** @param {string} fileName */
 export const runZipper = async (fileName = "workbook") => {
-  const psCommand = `Compress-Archive -Path * -DestinationPath ${resolve(
-    "out"
-  )}/${fileName}.zip`;
-  const res = await execPromise(psCommand, {
-    cwd: "temp",
-    shell: "powershell.exe",
-  });
+  if (platform() === "win32") {
+    const psCommand = `Compress-Archive -Path * -DestinationPath ${resolve(
+      "out"
+    )}/${fileName}.zip`;
+    const res = await execPromise(psCommand, {
+      cwd: "temp",
+      shell: "powershell.exe",
+    });
+  } else {
+    const psCommand = `zip -r ${resolve("out")}/${fileName}.zip ${resolve(
+      "temp"
+    )}`;
+    const res = await execPromise(psCommand);
+    console.log(res);
+  }
+
   await renameFile(`out/${fileName}.zip`, `out/${fileName}.xlsx`);
 };
