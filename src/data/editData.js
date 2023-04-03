c4ad6c665df1cf7f067987557983ce420b0aa5b5
chenp <chenpeleg@gmail.com>
2023-04-03 11:13:19 +0300

    build basic styles add another work sheet

--- src/data/editData.js
+++ src/data/editData.js
@@ -28,11 +28,11 @@ export const editData = async (data, config) => {
   const cells = [CellClass(4234), CellClass("I am a text"), CellClass(12)];
   const rows = [{ cells }, { cells }, { cells }];
   const worksheet = { rows, name: "worksheet1" };
+
   const sheet1 = buildSheetXml(worksheet);
   replaceInFile(
     resolve(config.tempDir, "xl", "worksheets", "sheet1.xml"),
     "<sheetData/>",
     sheet1
   );
-  console.log(sheet1);
 };
