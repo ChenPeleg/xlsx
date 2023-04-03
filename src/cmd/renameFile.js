dc1c972b58e9b7704c934fcf967239802a987a2c
chenp <chenpeleg@gmail.com>
2023-04-03 09:27:46 +0300

    formated files

--- src/cmd/renameFile.js
+++ src/cmd/renameFile.js
@@ -1,4 +1,4 @@
-import { rename } from 'node:fs/promises';
+import { rename } from "node:fs/promises";
 
 export const renameFile = async (oldPath, newPath) => {
   const res = await rename(oldPath, newPath);
