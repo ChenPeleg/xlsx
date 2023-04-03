f981c0a9c48bc9cb868b1f2b1b585205ff727ec7
chenp <chenpeleg@gmail.com>
2023-04-03 09:36:31 +0300

    refactored classes to types

--- src/data/editData.js
+++ src/data/editData.js
@@ -1,25 +1,33 @@
 import { resolve } from "node:path";
-import {
-  CellClass,
-  RowClass,
-  WorkbookClass,
-  Worksheet,
-} from "../types/worksheet.js";
 
 import { buildSheetXml } from "../functions/buildSheetXml.js";
 import { replaceInFile } from "../utils/replaceInFile.js";
+/**
+ * @param {any} value
+ * @returns {import("../types/worksheet.types.js").Cell}
+ */
+const CellClass = (value, options) => {
+  /** @type {"empty" | "string" | "number"} */
+  let dataType = "empty";
+  if (isNaN(value)) {
+    dataType = "string";
+  } else if (!value) {
+    dataType = "empty";
+  }
+  return {
+    dataType,
+    value,
+  };
+};
+
 /**
  * @param {any} data
  * @param {import("../types/config.type.js").XlsConfig} config
  */
 export const editData = async (data, config) => {
-  const cells = [
-    new CellClass(4234),
-    new CellClass("I am a text"),
-    new CellClass(12),
-  ];
-  const rows = [new RowClass(cells), new RowClass(cells), new RowClass(cells)];
-  const worksheet = new Worksheet(rows);
+  const cells = [CellClass(4234), CellClass("I am a text"), CellClass(12)];
+  const rows = [{ cells }, { cells }, { cells }];
+  const worksheet = { rows, name: "worksheet1" };
   const sheet1 = buildSheetXml(worksheet);
   replaceInFile(
     resolve(config.tempDir, "xl", "worksheets", "sheet1.xml"),
--- src/types/worksheet.types.ts
+++ src/types/worksheet.types.ts
@@ -1,13 +1,17 @@
+export interface Cell {
+    dataType: "number" | "string" | "empty";
+    value: number | string;
+    style?: any;
+}
 export interface Row {
   cells: Cell[];
 }
-export interface Cell {
-  dateType: "number" | "string" | "empty";
-  value: number | string;
-  style?: any;
-}
+
 export interface Sheet {
   rows: Row[];
   name: string;
 }
-export interface Workbook {}
+export interface Workbook {
+    name : string;
+    sheets : Sheet [];
+}
