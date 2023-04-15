735dddeb12d226a7a37c15c25ccaea1cd0831f0e
chenp <chenpeleg@gmail.com>
2023-04-15 23:11:49 +0300

    added test files path

--- test/test_runner.js
+++ test/test_runner.js
@@ -131,6 +131,9 @@ const getTapDataAsync = (testFiles) => {
 
 const testRunner = async (testType = "integration") => {
   const testFilesPath = `./test/${testType}`;
+
+  console.log("test files path", path.resolve(testFilesPath));
+
   try {
     const testFiles = (await getTestFiles(path.resolve(testFilesPath)))
       .filter((f) => f.includes("test.js"))
