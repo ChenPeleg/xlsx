c27dd22e778ab1fd4ad8d03a2e45bc431b7540fd
chenp <chenpeleg@gmail.com>
2023-04-16 21:37:12 +0300

    added test to the file size

--- test/integration/file-created.test.js
+++ test/integration/file-created.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
-import { readdir } from "fs";
+import { readdir, statSync } from "fs";
 import { resolve } from "node:path";
 import { runApp } from "../../src/core/main.js";
 /**
@@ -22,5 +22,7 @@ describe("an excel file was created", () => {
     const files = await getFolderFiles(resolve("out"));
     assert.equal(files.length, 1);
     assert.equal(files[0].includes(".xlsx"), true);
+    const stats = statSync(resolve("out", files[0]));
+    assert.equal(stats.size > 4000, true, "xlsx file too small");
   });
 });
