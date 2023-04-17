0fcd8a729a7e21caf5984d0678236704afb11c93
chenp <chenpeleg@gmail.com>
2023-04-17 12:34:06 +0300

    removed workbook xml

--- src/data/xlsxBuilder.js
+++ src/data/xlsxBuilder.js
@@ -11,23 +11,6 @@ export class xlsContent {
     return ["xl", "worksheets", `sheet${sheetIndex}.xml`];
   }
 
-  static workbookXml = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-    <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
-        xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"
-        xmlns:mx="http://schemas.microsoft.com/office/mac/excel/2008/main"
-        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
-        xmlns:mv="urn:schemas-microsoft-com:mac:vml"
-        xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
-        xmlns:x15="http://schemas.microsoft.com/office/spreadsheetml/2010/11/main"
-        xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac"
-        xmlns:xm="http://schemas.microsoft.com/office/excel/2006/main">
-        <workbookPr />
-        <sheets>
-            <sheet state="visible" name="sheet_1" sheetId="1" r:id="rId4" />
-        </sheets>
-        <definedNames />
-        <calcPr />
-    </workbook>`;
   static rels = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
   <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="theme/theme1.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/><Relationship Id="rId4" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/></Relationships>`;
   static styles = `<stylesheet xmls="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
@@ -54,7 +37,7 @@ export class xlsContent {
           }" />`
       )
       .join("\n");
-    return xlsContent.workbookXml.replace(
+    return xlsxFiles.workbookXml.content.replace(
       '<sheet state="visible" name="sheet_1" sheetId="1" r:id="rId4" />',
       allSheets
     );
