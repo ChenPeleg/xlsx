14feecdbf8fe39c3439dfb0cf8848ab85b216f70
chenp <chenpeleg@gmail.com>
2023-04-21 12:06:46 +0300

    tryinh to find the bug

--- src/core/main.js
+++ src/core/main.js
@@ -2,10 +2,16 @@ import { resolve } from "path";
 import { deleteFilesFromDir, runZipper } from "../utils/file-utils.js";
 import { xlsContent } from "../data/xlsxBuilder.js";
 import { buildSheetObject } from "../data/buildSheetObject.js";
+import { xlsxFiles } from "../data/xlsxFiles.js";
+import { googleSheetStyles } from "../data/BuildStyles.js";
 
 /** @param {{ tempDir: string }} config */
 
 export const runApp = async (data, config) => {
+  console.log(
+    "xlsxFiles.styles.content === googleSheetStyles",
+    xlsxFiles.styles.content === googleSheetStyles
+  );
   const tempDir = config?.tempDir || "temp";
   await deleteFilesFromDir(resolve(tempDir));
   const xlsObject = await buildSheetObject();
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ async function main() {
   await runApp(null, { tempDir: "temp" });
   console.log("success!");
 }
+
 main();
