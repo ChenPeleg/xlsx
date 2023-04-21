fd1b71e1ddb9ac5c5773dafdeafdd75bc7d8cc27
chenp <chenpeleg@gmail.com>
2023-04-21 14:02:36 +0300

    refactor before testing

--- src/core/main.js
+++ src/core/main.js
@@ -1,14 +1,16 @@
 import { resolve } from "path";
 import { deleteFilesFromDir, runZipper } from "../utils/file-utils.js";
 import { xlsContent } from "../data/xlsxBuilder.js";
-import { buildSheetObject } from "../data/buildSheetObject.js";
+import { buildExampleSheetsData } from "../data/buildSheetObject.js";
+import { createFileObjectFromSheets } from "../data/createXlsObject.js";
 
 /** @param {{ tempDir: string }} config */
-
 export const runApp = async (data, config) => {
   const tempDir = config?.tempDir || "temp";
   await deleteFilesFromDir(resolve(tempDir));
-  const xlsObject = await buildSheetObject();
+  /** @type {import("../types/worksheet.types.js").Workbook} */
+  const workbookObject = data || (await buildExampleSheetsData());
+  const xlsObject = createFileObjectFromSheets(...workbookObject.sheets);
   await xlsContent.copyFilesToTempDir(xlsObject, resolve(tempDir));
   await deleteFilesFromDir("out");
   await runZipper();
