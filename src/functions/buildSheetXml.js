25ed8348cd6472edc467495e63dc1c51318117c7
chenp <chenpeleg@gmail.com>
2023-04-21 08:42:49 +0300

    asdf

--- src/functions/buildSheetXml.js
+++ src/functions/buildSheetXml.js
@@ -6,13 +6,13 @@ import { columnIndexToLetter } from "./xlsxUtils.js";
  * @returns {string}
  */
 const buildCell = (cellIndex, cell) => {
-  const cellStyle = ""; // cell.style?.styleId ? `s="${cell.style.styleId}"` : "";
+  const cellStyle = cell.style?.styleId ? `s="${+cell.style.styleId + 1}"` : "";
   if (cell.dataType === "string") {
-    return `<c r="${cellIndex}" t="inlineStr" ${cellStyle}>  
+    return `<c r="${cellIndex}" ${""} t="inlineStr" >  
     <is><t>${cell.value}</t></is>  
   </c>`;
   }
-  return `<c r="${cellIndex}" ${cellStyle}>  
+  return `<c r="${cellIndex}" ${cellStyle} >  
    <v>${cell.value}</v>  
 </c>`;
 };
