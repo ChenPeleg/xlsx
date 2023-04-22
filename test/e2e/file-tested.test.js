59035cc5e9791a87185a4c9c935beae715f9e688
chenp <chenpeleg@gmail.com>
2023-04-22 18:18:51 +0300

    trying to open with excel js

--- test/e2e/file-tested.test.js
+++ test/e2e/file-tested.test.js
@@ -4,13 +4,20 @@ import { readdir, statSync } from "fs";
 import { resolve } from "node:path";
 import { runApp } from "../../src/core/main.js";
 import Excel from "exceljs";
+import { TestUtils } from "../integration/test-utils.js";
 
 describe("an excel file was created 2", () => {
   it("created an excel file", async () => {
     const fileName = "test-excel-file";
-    await runApp(null, { tempDir: "temp", outDir: "out" });
+    const firstCellValue = "first cell value";
+    const workbookData = await TestUtils.buildExampleSheetsData(fileName);
+    workbookData.sheets[0].rows[0].cells[0].value = firstCellValue;
+    await runApp(workbookData, { tempDir: "temp", outDir: "out" });
     var workbook = new Excel.Workbook();
     const file = await workbook.xlsx.readFile("out/test-excel-file.xlsx");
-    // assert.equal(file, 20, "xlsx file too small");
+
+    const worksheet = file.getWorksheet(0);
+    const cellValue = worksheet.findCell(1, 1).value;
+    assert.equal(cellValue, firstCellValue, "first cell value is correct");
   });
 });
