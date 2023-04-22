7a641c690a3831194ea964116803f87c3b58ff70
chenp <chenpeleg@gmail.com>
2023-04-22 18:39:07 +0300

    installing xlsx to try it out

--- src/core/main.js
+++ src/core/main.js
@@ -4,6 +4,7 @@ import { xlsContent } from "../data/xlsxBuilder.js";
 import { buildExampleSheetsData } from "../data/buildSheetObject.js";
 import { createFileObjectFromSheets } from "../data/createXlsObject.js";
 import Excel from "exceljs";
+import { realpathSync } from "node:fs";
 
 /**
  * @param {import("../types/worksheet.types.js").Workbook} data
@@ -16,8 +17,15 @@ export const runApp = async (data, config) => {
   const workbookObject = data || (await buildExampleSheetsData());
   const xlsObject = createFileObjectFromSheets(workbookObject);
   await xlsContent.copyFilesToTempDir(xlsObject, resolve(tempDir));
-  await deleteFilesFromDir(outDir);
-  await runZipper(workbookObject.name, outDir);
+  // await deleteFilesFromDir(outDir);
+  // await runZipper(workbookObject.name, outDir);
   const workbook = new Excel.Workbook();
-  const file = await workbook.xlsx.readFile("out/workbook.xlsx");
+  // await workbook.xlsx.readFile("out/workbook.xlsx");
+  // await workbook.calcProperties;
+  // const worksheet1 = workbook.getWorksheet("worksheet1");
+
+  const path = "out/workbook.xlsx";
+  await workbook.xlsx.readFile(resolve("out", "workbook.xlsx"));
+  console.log(workbook.worksheets);
+  // file.eachSheet((s) => console.log(s));
 };
