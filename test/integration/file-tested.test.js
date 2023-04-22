2bc20cd7788cf847787642d8e85762be1e6a0183
chenp <chenpeleg@gmail.com>
2023-04-22 20:04:53 +0300

    moved e2e test to the tests folder

--- test/integration/file-tested.test.js
+++ test/integration/file-tested.test.js
@@ -3,27 +3,22 @@ import assert from "node:assert/strict";
 import { readdir, statSync } from "fs";
 import { resolve } from "node:path";
 import { runApp } from "../../src/core/main.js";
-import Excel from "exceljs";
-import { TestUtils } from "../integration/test-utils.js";
+import { TestUtils } from "./test-utils.js";
 import * as XLSX from "xlsx/xlsx.mjs";
 import * as fs from "fs";
 XLSX.set_fs(fs);
 
-describe("an excel file was created 2", () => {
-  it("created an excel file", async () => {
+describe("test xlsx file data", () => {
+  it("text cell value is created correctly", async () => {
     const fileName = "test-excel-file";
     const firstCellValue = "first cell value";
     const workbookData = await TestUtils.buildExampleSheetsData(fileName);
     workbookData.sheets[0].rows[0].cells[0].value = firstCellValue;
     workbookData.sheets[0].rows[0].cells[0].dataType = "string";
     await runApp(workbookData, { tempDir: "temp", outDir: "out" });
-
     const workbook = await XLSX.readFile("out/test-excel-file.xlsx");
-    console.log(workbook);
-
     const worksheet1 = workbook.Sheets["worksheet1"];
-    let w = worksheet1;
-
-    assert.equal(worksheet1, 1, "first cell value is correct");
+    let value = worksheet1.A1.v;
+    assert.equal(value, firstCellValue, "first cell value is correct");
   });
 });
