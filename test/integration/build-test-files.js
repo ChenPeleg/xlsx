8983912a395b3913260982e5e582aedf2215bf21
chenp <chenpeleg@gmail.com>
2023-04-22 21:07:41 +0300

    asdgadfasdf

---
+++ test/integration/build-test-files.js
@@ -0,0 +1,8 @@
+const buildXlsxForTests = () => {
+  // const fileName = "test-excel-file";
+  // const firstCellValue = "first cell value";
+  // const workbookData = await TestUtils.buildExampleSheetsData(fileName);
+  // workbookData.sheets[0].rows[0].cells[0].value = firstCellValue;
+  // workbookData.sheets[0].rows[0].cells[0].dataType = "string";
+  // await runApp(workbookData, { tempDir: "temp", outDir: "out" });
+};
