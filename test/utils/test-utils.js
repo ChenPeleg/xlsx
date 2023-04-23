c76bff7394f5933430da79006d185cea9e4ba89d
chenp <chenpeleg@gmail.com>
2023-04-23 09:59:53 +0300

    added styles to example

--- test/utils/test-utils.js
+++ test/utils/test-utils.js
@@ -1,4 +1,9 @@
 export const TestUtils = {
+  /**
+   * @param {string | number} value
+   * @param {any} options
+   * @returns {import("../../src/types/worksheet.types.js").Cell}
+   */
   createCell(value, options) {
     return {
       value,
@@ -15,6 +20,8 @@ export const TestUtils = {
     const cell1 = createCell(10);
     const cell2 = createCell(20);
     const cell3 = createCell("Text");
+    const cell4 = (createCell(40).style.background = "#FF0000");
+    const cell5 = (createCell("Text With style").style.font.color = "#FF0000");
 
     const cells = [cell1, cell2, cell3];
     const rows = [{ cells }];
